b725dfcc93fd21f75ae5b8eb6af82f18a59abe90
Morten Larsen <mortenl41@gmail.com>
2025-10-14 20:22:27 +0200

    feat: implement tag helpers

---
+++ src/AspNet.AssetManager/LinkBundleTagHelper.cs
@@ -0,0 +1,67 @@
+// <copyright file="LinkBundleTagHelper.cs" company="Morten Larsen">
+// Copyright (c) Morten Larsen. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNet.AssetManager;
+
+/// <summary>
+/// A TagHelper responsible for rendering a link tag for CSS bundles.
+/// </summary>
+[OutputElementHint("link")]
+public class LinkBundleTagHelper(
+    IHtmlHelper htmlHelper,
+    IAssetService assetService,
+    ISharedSettings sharedSettings)
+    : TagHelper
+{
+    /// <summary>
+    /// Gets or sets the context information about the current view rendering process.
+    /// This property is automatically populated by the framework when the tag helper is used in a view.
+    /// It is used to access data, services, and other information related to the current rendering context.
+    /// </summary>
+    [ViewContext]
+    public required ViewContext ViewContext { get; set; }
+
+    /// <summary>
+    /// Gets or sets the fallback bundle name to be used if the primary bundle is unavailable.
+    /// </summary>
+    public string? Fallback { get; set; }
+
+    /// <summary>
+    /// Processes the tag helper and modifies the output to render a link tag for a CSS bundle.
+    /// </summary>
+    /// <param name="context">The context associated with the tag helper execution.</param>
+    /// <param name="output">The output that will be modified for rendering the link tag.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        ArgumentNullException.ThrowIfNull(output);
+
+        if (htmlHelper is IViewContextAware aware)
+        {
+            aware.Contextualize(ViewContext);
+        }
+
+        output.TagName = "link";
+        output.TagMode = TagMode.SelfClosing;
+
+        var bundle = ViewContext.ViewData.GetBundleName() ?? htmlHelper.GetBundleName();
+        var file = await assetService.GetLinkHref(bundle, Fallback).ConfigureAwait(false);
+
+        output.Attributes.SetAttribute("href", $"{sharedSettings.AssetsWebPath}{file}");
+
+        output.Attributes.SetAttribute("rel", "stylesheet");
+
+        if (sharedSettings.DevelopmentMode)
+        {
+            output.Attributes.SetAttribute("crossorigin", "anonymous");
+        }
+    }
+}
---
+++ src/AspNet.AssetManager/ScriptBundleTagHelper.cs
@@ -0,0 +1,96 @@
+// <copyright file="ScriptBundleTagHelper.cs" company="Morten Larsen">
+// Copyright (c) Morten Larsen. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNet.AssetManager;
+
+/// <summary>
+/// A TagHelper implementation that generates a "script" tag for a JavaScript bundle,
+/// with support for fallback, async, and defer attributes.
+/// </summary>
+[OutputElementHint("script")]
+public class ScriptBundleTagHelper(
+    IHtmlHelper htmlHelper,
+    IAssetService assetService,
+    ISharedSettings sharedSettings)
+    : TagHelper
+{
+    /// <summary>
+    /// Gets or sets the current view context, which encapsulates information about the current view execution,
+    /// including view data, temporary data, and other context-specific settings.
+    /// </summary>
+    [ViewContext]
+    public required ViewContext ViewContext { get; set; }
+
+    /// <summary>
+    /// Gets or sets the fallback value used when a JavaScript bundle source cannot be resolved.
+    /// </summary>
+    public string? Fallback { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the script tag should include the "async" attribute.
+    /// </summary>
+    public bool Async { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the script should be deferred until the HTML document has been parsed.
+    /// When set to true, the "defer" attribute will be added to the generated script tag.
+    /// </summary>
+    public bool Defer { get; set; }
+
+    /// <summary>
+    /// Processes the tag helper to generate a script tag for a JavaScript bundle.
+    /// </summary>
+    /// <param name="context">The context for the tag helper execution.</param>
+    /// <param name="output">The output object used to generate the HTML content.</param>
+    /// <returns>A task that represents the asynchronous operation of processing the tag helper.</returns>
+    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+    {
+        ArgumentNullException.ThrowIfNull(output);
+
+        if (output.IsContentModified)
+        {
+            return;
+        }
+
+        if (htmlHelper is IViewContextAware aware)
+        {
+            aware.Contextualize(ViewContext);
+        }
+
+        output.TagName = "script";
+        output.TagMode = TagMode.StartTagAndEndTag;
+
+        var bundle = ViewContext.ViewData.GetBundleName() ?? htmlHelper.GetBundleName();
+        var file = await assetService.GetScriptSrc(bundle, Fallback).ConfigureAwait(false);
+
+        output.Attributes.SetAttribute("src", $"{sharedSettings.AssetsWebPath}{file}");
+
+        if (sharedSettings.DevelopmentMode)
+        {
+            if (sharedSettings.ManifestType == ManifestType.Vite)
+            {
+                output.Attributes.SetAttribute("type", "module");
+            }
+
+            output.Attributes.SetAttribute("crossorigin", "anonymous");
+        }
+
+        if (Async)
+        {
+            output.Attributes.SetAttribute(new TagHelperAttribute("async"));
+        }
+
+        if (Defer)
+        {
+            output.Attributes.SetAttribute(new TagHelperAttribute("defer"));
+        }
+    }
+}
